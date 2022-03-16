0d5e3f00ef01a0721cdd9ac706ffb111ff9c2b7d
Jun Won Oh <junoh9847@gmail.com>
2022-03-15 23:25:42 -0400

    connected to mongodb atlas, set to update user verified status on interaction

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from pprint import pprint
 import discord
-import random
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 client = discord.Client()
+mongo_client = MongoClient('mongodb+srv://ieeeserver:'f'{MONGO_PASSWORD}''@cluster0.v7iyp.mongodb.net/ieeeserverDB?retryWrites=true&w=majority')
+db = mongo_client.admin
+serverStatusResult = db.command("serverStatus")
+pprint(serverStatusResult)
+
+ieee_db = mongo_client["ieeeserverDB"]
+ieee_user_db = ieee_db["users"]
+update_verified_status = {"$set": {"verifiedUser": True}}
 
 @client.event
 async def on_ready():
@@ -20,6 +30,8 @@ async def on_message(message):
     print(message)
     if channel == 'general':
         if message.content == '!verify':
+            user_query = {"id": message.author.id}
+            ieee_user_db.update_one(user_query, update_verified_status)
             await message.author.send(f'Hello, {username}!\n'
                                       f'Your SSH login is is: ```Username: \n'
                                       f'Password:```\n'
