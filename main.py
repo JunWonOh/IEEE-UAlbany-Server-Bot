2d5d7129b77f1327633cbb3a2d5c13f737914d45
Jun Won Oh <junoh9847@gmail.com>
2022-03-28 01:36:07 -0400

    bot can now create conda environments and edit .bashrc

--- main.py
+++ main.py
@@ -33,24 +33,35 @@ async def on_message(message):
     print(message)
     # add 'and discord.utils.get(message.author.roles, name=role_name) is None'
     if channel == 'general':
-        role_name = 'Something'
+        role_name = 'Server Contributor'
         if message.content == '!verify':
             user_query = {"discord_id": str(message.author.id)}
             print('id: ' + str(message.author.id))
             # assign messenger role
             await message.author.add_roles(discord.utils.get(message.guild.roles, name=role_name))
+            await message.author.send(f'Hello, {username}! We are processing your account on the server, '
+                                      f'please hold for another message!')
             ubuntu_username = username.lower().replace(" ", "") + tag
             ubuntu_password = secrets.token_hex(16)
-            # ubuntu create user
+            # create user on server
             await os.system(f'sudo useradd -m {ubuntu_username}')
             await os.system(f'sudo passwd {ubuntu_username}')
             # 2x - once to set and once to verify
             await os.system(f'{ubuntu_password}')
             await os.system(f'{ubuntu_password}')
+            # set up user's conda environment
+            await os.system(f'cd /home/{ubuntu_username}')
+            await os.system(f'conda create -n {ubuntu_username}')
+            await os.system(f'conda activate {ubuntu_username}')
+            # edit user bashrc to load conda venv on ssh login
+            await os.system(f'echo \'if [[ -n $SSH_CONNECTION ]] ; then\' >> .bashrc')
+            await os.system(f'echo \'conda activate ubuntu_username\' >> .bashrc')
+            await os.system(f'echo \'fi\' >> .bashrc')
 
             # update on the database that user has been verified
             ieee_user_db.update_one(user_query, update_verified_status)
-            await message.author.send(f'Hello, {username}!\n'
+            # DM the user instructions
+            await message.author.send(f'Thank you for waiting!\n'
                                       f'Your SSH login is: ```Username: {ubuntu_username}\n'
                                       f'Password: {ubuntu_password}```\n'
                                       f'Please do not share this with anyone!')
