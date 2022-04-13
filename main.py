949edd752a2a3d5c049df4537dc161cabc0c26bf
JunWonOh <junoh9847@gmail.com>
2022-04-12 23:29:57 -0400

    status messages added for server

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import discord
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+SUDO_PASSWORD = os.getenv('SUDO_PASSWORD')
 client = discord.Client()
 mongo_client = MongoClient('mongodb+srv://ieeeserver:'f'{MONGO_PASSWORD}''@cluster0.v7iyp.mongodb.net/ieeeserverDB?retryWrites=true&w=majority')
 db = mongo_client.admin
@@ -35,28 +36,42 @@ async def on_message(message):
     if channel == 'general':
         role_name = 'Server Contributor'
         if message.content == '!verify':
+            print(f'{username} (id: {message.author.id}) is requesting verification')
             user_query = {"discord_id": str(message.author.id)}
-            print('id: ' + str(message.author.id))
             # assign messenger role
             await message.author.add_roles(discord.utils.get(message.guild.roles, name=role_name))
             await message.author.send(f'Hello, {username}! We are processing your account on the server, '
                                       f'please hold for another message!')
             ubuntu_username = username.lower().replace(" ", "") + tag
             ubuntu_password = secrets.token_hex(16)
-            # create user on server
-            await os.system(f'sudo useradd -m {ubuntu_username}')
-            await os.system(f'sudo passwd {ubuntu_username}')
+            print(f'generated username and password for {username}')
+
+            # create user on server with m flag to create respective directory
+            print(f'creating user {username} in server...')
+            os.system(f'sudo useradd -m {ubuntu_username}')
+
+            # set password for user
+            os.system(f'sudo passwd {ubuntu_username}')
+            # enter sudo account password
+            print('giving bot sudo permissions...')
+            os.system(f'{SUDO_PASSWORD}')
             # 2x - once to set and once to verify
-            await os.system(f'{ubuntu_password}')
-            await os.system(f'{ubuntu_password}')
+            print(f'setting user password {username} in server...')
+            os.system(f'{ubuntu_password}')
+            print(f'verifying user password {username} in server...')
+            os.system(f'{ubuntu_password}')
+
             # set up user's conda environment
-            await os.system(f'cd /home/{ubuntu_username}')
-            await os.system(f'conda create -n {ubuntu_username}')
-            await os.system(f'conda activate {ubuntu_username}')
+            print(f'creating and setting conda environment for {username}...')
+            os.system(f'cd /home/{ubuntu_username}')
+            os.system(f'conda create -n {ubuntu_username}')
+            os.system(f'conda activate {ubuntu_username}')
+
             # edit user bashrc to load conda venv on ssh login
-            await os.system(f'echo \'if [[ -n $SSH_CONNECTION ]] ; then\' >> .bashrc')
-            await os.system(f'echo \'conda activate ubuntu_username\' >> .bashrc')
-            await os.system(f'echo \'fi\' >> .bashrc')
+            print(f'setting bashscript for {username}...')
+            os.system(f'echo \'if [[ -n $SSH_CONNECTION ]] ; then\' >> .bashrc')
+            os.system(f'echo \'conda activate ubuntu_username\' >> .bashrc')
+            os.system(f'echo \'fi\' >> .bashrc')
 
             # update on the database that user has been verified
             ieee_user_db.update_one(user_query, update_verified_status)
