23e877756f33b9c2149e867c9bba56db76eedc14
JunWonOh <junoh9847@gmail.com>
2022-04-17 00:59:51 -0400

    fixes to ubuntu compatibility

--- main.py
+++ main.py
@@ -60,9 +60,10 @@ async def on_message(message):
 
             # edit user bashrc to load conda venv on ssh login
             print(f'setting bashscript for {username}...')
-            os.system(f'echo \'if [[ -n $SSH_CONNECTION ]] ; then\' >> .bashrc')
-            os.system(f'echo \'conda activate ubuntu_username\' >> .bashrc')
-            os.system(f'echo \'fi\' >> .bashrc')
+            os.system(f'cd /home/{ubuntu_username} && '
+                      f'echo \'if [[ -n $SSH_CONNECTION ]] ; then \n '
+                      f'conda activate {ubuntu_username} \n '
+                      f'fi\' >> .bashrc')
 
             # update on the database that user has been verified
             ieee_user_db.update_one(user_query, update_verified_status)
